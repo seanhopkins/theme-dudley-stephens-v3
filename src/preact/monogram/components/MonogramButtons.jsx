6db581e89ff82e6cb55a9bc97fd6e1d22f6468c9
Sean Hopkins <48530536+seanhopkins@users.noreply.github.com>
2023-08-18 18:10:04 -0400

    add block option to item containing kids

--- src/preact/monogram/components/MonogramButtons.jsx
+++ src/preact/monogram/components/MonogramButtons.jsx
@@ -5,10 +5,26 @@ const MonogramButtons = ({ data, style, setStyle }) => {
   const visibleMonogramVariants = data.monogram_product.variants.filter(
     (variant) =>
       handleize(variant.title).includes("block") &&
-      handleize(data.parent_product.type) !== "kids"
+      !handleize(data.parent_product.type).includes("kids")
         ? false
         : true
   );
+
+
+  // FYI, if it's easier to process, lines 5 => 11 could be rewrriten like this:
+  // const visibleMonogramVariants = data.monogram_product.variants.filter(
+  //   (variant) => {
+
+  //     if (
+  //       handleize(variant.title).includes("block") &&
+  //       !handleize(data.parent_product.type).includes("kids")
+  //     ) {
+  //       return false;
+  //     } else {
+  //       return true;
+  //     }
+  //   }
+  // );
 
   return (
     <div className="MonogramButtons">
